23210bc6330b9d7fede113dd4af075538057659a
unknown <2m.laclef@gmail.com>
2025-02-14 15:33:06 +0000

    Correction noms de fichiers

--- src/componenent/Cards.jsx
+++ src/componenent/Cards.jsx
@@ -1,42 +1,40 @@
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
 import "./Cards.css";
 
 function Cards() {
-    return (
-      <div className="cartes">
-        <div>
-          <Card className="cart" style={{ width: "27rem" }}>
-            <Card.Img variant="top" src="public/plat1.jpg" />
-            <Card.Body>
-              <Card.Title>Saumon en Croûte</Card.Title>
-              <Card.Text>
-                Saumon frais, épinards, sauce hollandaiseSaumon frais, épinards,
-                sauce hollandaise
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div>
-          <Card className="cart" style={{ width: "27rem" }}>
-            <Card.Img variant="top" src="public/plat2.jpg" />
-            <Card.Body>
-              <Card.Title>Filet Mignon</Card.Title>
-              <Card.Text>Sauce au poivre, légumes de saison</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-        <div>
-          <Card className="cart" style={{ width: "27rem" }}>
-            <Card.Img variant="top" src="public/plat2.jpg" />
-            <Card.Body>
-              <Card.Title>Dessert du Chef</Card.Title>
-              <Card.Text>Création unique quotidienne</Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
-    );
+  return (
+    <div className="cartes">
+      {/* Carte 1 */}
+      <Card className="cart" style={{ width: "100%" }}>
+        <Card.Img variant="top" src="plat1.jpg" alt="Saumon en Croûte" />
+        <Card.Body>
+          <Card.Title>Saumon en Croûte</Card.Title>
+          <Card.Text>
+            Saumon frais, épinards, sauce hollandaiseSaumon frais, épinards,
+            sauce hollandaise
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      {/* Carte 2 */}
+      <Card className="cart" style={{ width: "100%" }}>
+        <Card.Img variant="top" src="plat2.jpg" alt="Filet Mignon" />
+        <Card.Body>
+          <Card.Title>Filet Mignon</Card.Title>
+          <Card.Text>Sauce au poivre, légumes de saison</Card.Text>
+        </Card.Body>
+      </Card>
+
+      {/* Carte 3 */}
+      <Card className="cart" style={{ width: "100%" }}>
+        <Card.Img variant="top" src="plat2.jpg" alt="Dessert du Chef" />
+        <Card.Body>
+          <Card.Title>Dessert du Chef</Card.Title>
+          <Card.Text>Création unique quotidienne</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Cards;
--- src/componenent/Home.jsx
+++ src/componenent/Home.jsx
@@ -5,28 +5,33 @@ import Boutton from "./Boutton";
 const Home = () => {
   return (
     <div>
+      {/* Banner Section */}
       <div className="banner">
         <h1>La Belle Cuisine</h1>
         <p>Une expérience gastronomique exceptionnelle</p>
         <Boutton />
       </div>
+
+      {/* Presentation Section */}
       <div className="container-presentation">
         <div className="chef">
-          <img src="public/3.png" alt="Chef Étoilé" />
+          <img src="3.png" alt="Chef Étoilé" />
           <h3>Chef Étoilé</h3>
           <p>Cuisine raffinée par notre chef renommé</p>
         </div>
         <div className="horaires">
-          <img src="public/2.png" alt="Horaires" />
+          <img src="2.png" alt="Horaires" />
           <h3>Horaires</h3>
           <p>Ouvert du Mardi au Dimanche</p>
         </div>
         <div className="emplacement">
-          <img src="public/4.png" alt="Emplacement" />
+          <img src="4.png" alt="Emplacement" />
           <h3>Emplacement</h3>
           <p>Au cœur de la ville</p>
         </div>
       </div>
+
+      {/* Specialities Section */}
       <div className="specialite">
         <h1>Nos spécialités</h1>
       </div>
